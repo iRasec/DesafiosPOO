2dbae69e3d4aff24b3aeca0da3e38214b838ed79
iRasec <luancesar@alunos.utfpr.edu.br>
2025-08-22 16:21:18 -0300

    Desafio classhero

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿double n1 = 0.1;
+double n2 = 0.2;
+double n3 = n1 + n2;
+
+Console.WriteLine($"0.1 + 0.2 = {n3}");
+Console.WriteLine($"0.3       = {0.3}");
+Console.WriteLine($"São iguais? {n3 == 0.3}");
