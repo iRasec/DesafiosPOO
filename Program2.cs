df2c1637ba41ce2da39d568037ecc82ba2f8f259
iRasec <luancesar@alunos.utfpr.edu.br>
2025-08-25 21:20:13 -0300

    Segundo desafio do classhero

---
+++ Program2.cs
@@ -0,0 +1,14 @@
+Console.WriteLine("Digite sua idade:");
+int idade = int.Parse(Console.ReadLine());
+if (idade < 12)
+{
+    Console.WriteLine("Criança");
+}
+else if (idade >= 12 && idade <= 17)
+{
+    Console.WriteLine("Adolescente");
+}
+else
+{
+    Console.WriteLine("Adulto");
+}
